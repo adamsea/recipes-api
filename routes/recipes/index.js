4fdc977bc769b001a0d134bf3f4656e39898a807
Eric Adams <adams.ea@gmail.com>
2015-10-11 18:06:53 -0400

    Add recipe listing with optional expansion of properties

--- routes/recipes/index.js
+++ routes/recipes/index.js
@@ -1,11 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var JsonDB = require('node-json-db');
+var _ = require('lodash');
 
 // Recipes listing
 router.get('/', function (req, res, next) {
   var db = new JsonDB('db', false, false);
   var recipes = db.getData('/recipes');
+
+  // Expand requested resources if they exist
+  // Users and ingredients for the recipes are loaded
+  if (!_.isUndefined(req.query._expand)) {
+    var users = db.getData('/users');
+    _(recipes)
+      .forEach(function (recipe) {
+        recipe.user = _(users).findWhere({ id: recipe.userId });
+        delete recipe.userId;
+      })
+      .value();
+  }
+
   res.json(recipes);
 });
 
