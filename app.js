fc72e65f042428229b416b543363181fd4564498
Eric Adams <adams.ea@gmail.com>
2015-10-11 18:06:53 -0400

    Add recipe listing with optional expansion of properties

--- app.js
+++ app.js
@@ -17,16 +17,21 @@ app.use('/', routes);
 app.use('/recipes', recipes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handlers
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  // Send generic error response here
+app.use(function (err, req, res, next) {
+  var status = err.status || 500;
+  if (status === 404) {
+    res.status(status).json({ message: 'We were unable to locate that resource' });
+  }
+  else {
+    res.status(status).json({ message: 'There was an error on the server' });
+  }
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 // Root api entry point
-router.get('/', function(req, res, next) {
-    res.send('Hello Recipes API!');
+router.get('/', function (req, res, next) {
+  res.send('Hello Recipes API!');
 });
 
 module.exports = router;
