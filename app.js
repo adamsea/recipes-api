44f8721b3f84d5f4476bd6d04890198338e05dc7
Eric Adams <adams.ea@gmail.com>
2015-10-10 22:34:09 -0400

    Initial setup of express app with dependencies

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+// Modules for API resource routes
+var routes = require('./routes/index');
+
+// Create express app instance and set middleware
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Mount the route modules
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  // Send generic error response here
+});
+
+module.exports = app;
