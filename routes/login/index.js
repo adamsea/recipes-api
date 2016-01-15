6732c4705caf7816423bc42c6f9b7db9079e61d6
Eric Adams <adams.ea@gmail.com>
2016-01-14 20:48:07 -0500

    Add section 6.1 code to provide login for users

--- routes/login/index.js
+++ routes/login/index.js
@@ -1,7 +1,46 @@
 var express = require('express');
 var router = express.Router();
+var auth = require('../../utils/auth');
+var _ = require('lodash');
+var JsonDB = require('node-json-db');
+var crypto = require('crypto');
 
 // Login a user with basic auth
-router.post('/', function (req, res, next) {});
+router.post('/', auth.basic, function (req, res, next) {
+  var db = new JsonDB('db', false, true);
+  var users = db.getData('/users');
+
+  function sendError() {
+    res.status(500).json({
+      success: false,
+      message: 'Unable to authenticate user.'
+    });
+  }
+
+  // Find the auth user to update last login
+  var i = _.findIndex(users, {username: req.user.username});
+  crypto.randomBytes(16, function(err, buf) {
+    if (!err) {
+      var ret = _.attempt(db.push.bind(db), '/users['+i+']', {
+        last_login: _.now(),
+        token: buf.toString('hex')
+      }, false);
+
+      if (!_.isError(ret)) {
+        res.json({
+          success: true,
+          message: 'Successfully authenticated user.',
+          token: buf.toString('hex')
+        });
+      }
+      else {
+        sendError();
+      }
+    }
+    else {
+      sendError();
+    }
+  });
+});
 
 module.exports = router;
